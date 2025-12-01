d1322744a5573359b131a0af2db553014ee68ffc
Nour-Zouari <153989236+Nour-Zouari@users.noreply.github.com>
2025-12-01 20:46:23 +0100

    charger les noeuds pour voir l'importance de l'algo load balancer

--- src/launch.bat
+++ src/launch.bat
@@ -1,20 +1,40 @@
 @echo off
+
+REM =============================
 REM Lancer les nœuds fog
+REM =============================
 start cmd /k "python src/fog_node.py"
 start cmd /k "python src/fog_node2.py"
 start cmd /k "python src/fog_node3.py"
 
+REM =============================
 REM Choix de l'algorithme pour le load balancer
-echo Choisir l'algorithme pour le load balancer :
+REM =============================
+echo Choisir l'algorithme :
 echo 1) Algo 
 echo 2) Random
-set /p choice=Votre choix (1 ou 2) : 
+set /p choix=Votre choix (1 ou 2) : 
 
-if "%choice%"=="1" (
+if "%choix%"=="1" (
     start cmd /k "python load_balancer_algo.py"
 ) else (
     start cmd /k "python load_balancer_random.py"
 )
 
-REM Lancer le client
+REM =============================
+REM Lancer le client (interface HTML)
+REM =============================
 start cmd /k "python src/client.py"
+
+REM =============================
+REM Ajouter une charge CPU locale pour rendre les nœuds plus sollicités
+REM (sans toucher au code Python et sans ouvrir plusieurs clients)
+REM =============================
+echo Simulation de charge CPU pour charger les noeuds et voir l'effet de leurs collaboration pour le chiffrement
+
+REM lancer 2 tâches de charge CPU en arrière-plan pour charger les noeuds et voir l'effet de leurs collaboration pour le chiffrement
+start /B cmd /c "for /L %%x in (1,1,999999999) do @set /a %%x*%%x >nul"
+start /B cmd /c "for /L %%x in (1,1,999999999) do @set /a %%x*%%x >nul"
+
+echo Tout est lance. Utiliser l'interface HTML pour choisir et envoyer le fichier.
+pause
